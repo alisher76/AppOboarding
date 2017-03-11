545ec6508421d5e54c1ca07e9e50423d54b55be6
Ali•Gorithm <alisher.coder@gmail.com>
2017-03-11 15:01:45 -0500

    first comiit

---
+++ AppOnboarding/OnboardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingView.swift
+//  AppOnboarding
+//
+//  Created by Alisher Abdukarimov on 3/10/17.
+//  Copyright © 2017 MrAli•Gorithm. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingView: PaperOnboarding {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
